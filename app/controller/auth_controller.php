60d424b81f916a6ed22862917f989f69de4db2d0
sena <atharizkysena@gmail.com>
2025-11-22 22:45:25 +0700

    update code Auth_controller.php => menggunakan auth_models untuk query ke database

--- app/controller/auth_controller.php
+++ app/controller/auth_controller.php
@@ -1,37 +1,39 @@
 <?php
-include '../config/db_conn.php';
+require_once '../config/db_conn.php';
 session_start();
 
 $nidn = $_POST['nidn'];
 $password = $_POST['pass'];
 
-$stmt = $conn->prepare("SELECT nidn, pw, nama FROM adminlog WHERE nidn = ?");
-$stmt->bind_param("s", $nidn);
-$stmt->execute();
-$stmt->store_result();
+require_once '../models/auth_model.php';
+
+$stmt = getAdminByNidn($conn, $nidn);
 
 if ($stmt->num_rows === 1) {
-    $stmt->bind_result($db_nidn, $db_pw, $db_nama); 
+    // Mencocokkan Data User
+    $stmt->bind_result($db_nidn, $db_pw, $db_nama, $db_id);
     $stmt->fetch();
 
+    // Logika Jika Password Cocok
     if ($password === $db_pw) {
         $_SESSION['nidn'] = $db_nidn;
         $_SESSION['nama'] = $db_nama;
+        $_SESSION['user_id'] = $db_id;
 
-        $_SESSION['login_success'] = "Selamat Datang " . $_SESSION['nama'] ."!";
-
+        $_SESSION['login_success'] = "Selamat Datang " . $_SESSION['nama'] . "!";
         header("Location: ../../public/admin/penerbitan.php");
         exit();
     } else {
-        $_SESSION['login_error'] = "NIDN atau Password salah.";
+        $_SESSION['login_error'] = "NIDN atau Password Salah.";
         header("Location: ../../public/admin/loginpage.php");
         exit();
     }
 
+} else {
+    $_SESSION['login_error'] = "NIDN atau Password Salah.";
+    header("Location: ../../public/admin/loginpage.php");
+    exit();
 }
-$stmt -> close();
-$conn -> close();
 
-?>
-   
-
+$stmt->close();
+$conn->close();
